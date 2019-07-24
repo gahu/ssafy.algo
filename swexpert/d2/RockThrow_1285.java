22b702b911dacfd30e999f8973347093b2eff6b2
gahusb <bgg8988@gmail.com>
2019-07-24 18:22:56 +0900

    first commit

---
+++ swexpert/d2/RockThrow_1285.java
@@ -0,0 +1,18 @@
+package d2;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class RockThrow_1285 {
+	public static void main(String[] args) throws FileNotFoundException {
+		System.setIn(new FileInputStream(""));
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int[] people;
+		for (int t = 1; t <= T; t++) {
+			int N = sc.nextInt();
+			people = new int[N];
+		}
+	}
+}
